b9fa21bc1227505e8afe2b00e74a428e65416306
NikolaMeshkov <104001044+NikolaMeshkov@users.noreply.github.com>
2022-04-20 12:56:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-
+//zadaca1 Напиши програм кој како влез зема два цели броја и ги споредува. Потоа го испечатува
+//поголемиот број проследен со коментарот e pogolem od и го испечатува и помалиот број. Ако броевите се еднакви, програмот треба да испечати Broevite se ednakvi.
 #include <stdio.h>
 int broj1;
 int broj2;
@@ -17,7 +18,7 @@ int main()
 
 
 }
-// zadaca2 // 
+// zadaca2 Напиши програм кој вчитува два цели броја и одредува дали се парни или непарни 
 #include <stdio.h>
 int broj1;
 int broj2;
@@ -36,5 +37,29 @@ int main()
         printf ("%d e paren",broj2);
     else
         printf("%d e neparen",broj2);
+}
+//zadaca 3 Обидете се со спецификаторот за конверзија %d да испечатите букви на следниот начин:
+//printf(“%d”, ‘A’) /* karakteri se koristat vo edinecni navodnici */
+//Што се случува кога се печатат следните карактери: b  c  0  1  2  $  *  +  / и празно место?
+// 
+printf(“%d”, ‘A’)  //dava rezultat 65 na b dava 98, na c 99, na 0 48, na 1 49, na 2 50, na $ 36, na prazno mesto 32, na* 42, na + 43 ANSI character set
+    
+//zadaca 4
+    //Во примерот p03_01.c, во наредбата printf наместо спецификаторот за конверзија  %f, поставете го спецификаторот %d. Што се случува и зошто?
+   dava rezultat 1073741824 bidejki %d e za integer(cel broj) a rezultatot e decimalen broj (float)
+    
+//zadaca 5
+    //Напиши програма која внесува радиус на круг и испечатува негова плоштина и периметар. За вредност на пи да се земе 3.14.
+    
+#include <stdio.h>
+float r, pi=3.14;
+int main()
+{
+printf ("Vnesi radius na krug");
+scanf ("%f",&r);
+
+printf("perimetarot na krug so radius %f e %f\n",r,2*r*pi);
+printf("plostinata na krug si radius %f e %f\n",r,r*r*pi);
+
 }
 
