23579b6e3ea14eb3d4da575f99c8fd02ce4384fb
NikolaMeshkov <104001044+NikolaMeshkov@users.noreply.github.com>
2022-05-06 13:34:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1445,6 +1445,38 @@ void mnoziSoStepenOdDva( unsigned br, unsigned st, unsigned rez)
 }
 
 
+// zadaca 3 3. Да се открие и опише што работи следниот програм?
+/* Program vezba11_01.c
+   Shto raboti sledniot program?
+#include <stdio.h>
+ 
+int misterija( unsigned bitovi ); 
+ 
+int main()
+{ 
+   unsigned x; 
+   printf( "Vnesi cel broj: " );
+   scanf( "%u", &x );
+   printf( "Rezultatot e %d\n", misterija( x ) );
+   return 0; 
+} 
+ 
+int misterija( unsigned bitovi )
+{ 
+   unsigned i;              
+   unsigned maska = 1 << 31; 
+   unsigned total = 0;      
+ 
+   for ( i = 1; i <= 32; i++, bitovi <<= 1 ) {
+      if ( ( bitovi & maska ) == maska ) { 
+         total++;
+      } 
+   } 
+   return !( total % 2 ) ? 1 : 0;
+} */
+
+// vrakja dali zbirot na edinicite vo brojot e paren ili nepare, za paren 1 za neparen 0
+
 
 
 
